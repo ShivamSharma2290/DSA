10c40700dbdd3ccd503e7d080bc5e8dc6794e7da
Shivam Sharma <shivsharma4108@gmail.com>
2025-03-20 16:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (18.60%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,39 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backTrack(nums.length-1, nums,  res,new ArrayList<>());
+        backTrack(0, nums,  res,new ArrayList<>());
         return res; 
     }
-    public void backTrack(int n,int[] nums,List<List<Integer>> res,List<Integer> temp)
+    public void backTrack(int start,int[] nums,List<List<Integer>> res,List<Integer> temp)
     {
-        if(n==-1)
-        {
-            res.add(new ArrayList<>(temp));
-            return;
-        }
-        temp.add(nums[n]);
-        backTrack(n-1,nums,res,temp);
-        temp.remove(temp.size() - 1);
-        backTrack(n-1,nums,res,temp);
+         res.add(new ArrayList<>(temp));
+         for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backTrack(i + 1, nums, res, temp);
+            temp.remove(temp.size() - 1);
+         }
     }
-    
-
-   
 }
-/*
-public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack(0, nums, new ArrayList<>(), res);
-        return res;
-    }
-
-    private void backtrack(int start, int[] nums, List<Integer> temp, List<List<Integer>> res) {
-        res.add(new ArrayList<>(temp));
-
-        for (int i = start; i < nums.length; i++) {
-            temp.add(nums[i]);         
-            backtrack(i + 1, nums, temp, res);
-            temp.remove(temp.size() - 1); 
-        }
-    }
-*/
