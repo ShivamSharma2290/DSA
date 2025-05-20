327c5cfa74f17341b1e380057eb2cae0f1cab935
Shivam Sharma <shivsharma4108@gmail.com>
2025-05-21 00:59:32 +0530

    Time: 33 ms (20.22%) | Memory: 46.2 MB (5.96%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,6 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
+        /*greedy
+        Arrays.sort(coins);
+
+        
+        int c=0;
+        int n=coins.length;
+        int i=n-1;
+        while(i>-1 && amount>0)
+        {
+            if(amount==0)
+            {
+                return c;
+            }
+            if(coins[i]>amount)
+            {
+                i--;
+                continue;
+            }
+            c+=amount/coins[i];
+            amount=amount%coins[i];
+            i--;
+
+        }
+        if(amount>0)return -1;
+        return c;
+        /*
+        /*int[] dp = new int[amount + 1];
 
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
@@ -13,6 +39,23 @@ class Solution {
             }
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];*/
+        int [][]dp=new int[coins.length][amount+1];
+        for(int d[]:dp)Arrays.fill(d,-1);
+        int ans=rec(0,coins,amount,dp);
+        return ans>amount?-1:ans;
+    }
+
+    public int rec(int i,int[] coins,int amount,int dp[][])
+    {
+        if(amount==0)return 0;
+        if(i==coins.length) return Integer.MAX_VALUE-amount-1;
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        if(coins[i]>amount)
+        {
+            return rec(i+1,coins,amount,dp);
+        }
+
+        return dp[i][amount]=Math.min(1+rec(i,coins,amount-coins[i],dp),rec(i+1,coins,amount,dp));
     }
 }
