b166a52a4c29cbf755953cb28aad7f7dcaf032da
Shivam Sharma <shivsharma4108@gmail.com>
2025-05-29 13:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (8.99%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        //only odd number of bulbs are on.
+        // 1,4,9 are perfect suare.
+        return (int)Math.sqrt(n);
+        
+    }
+}
